fccfe0bed8d1506c98569061735aa00aed683f5d
teresanguyen01 <teresatamnguyen20@gmail.com>
2026-03-28 22:05:31 -0400

    s

--- hardware/servo_test.py
+++ hardware/servo_test.py
@@ -4,24 +4,24 @@ import time
 kit = ServoKit(channels=16, address=0x40)
 
 # center both
-kit.servo[0].angle = 90
-kit.servo[1].angle = 90
+kit.servo[0].angle = 0
+kit.servo[1].angle = 0
 time.sleep(2)
 
 # servo 0
-kit.servo[0].angle = 80
+kit.servo[0].angle = 180
 time.sleep(1)
-kit.servo[0].angle = 100
+kit.servo[0].angle = 0
 time.sleep(1)
-kit.servo[0].angle = 90
+kit.servo[0].angle = 180
 time.sleep(1)
 
 # servo 1
-kit.servo[1].angle = 80
+kit.servo[1].angle = 180
 time.sleep(1)
-kit.servo[1].angle = 100
+kit.servo[1].angle = 0
 time.sleep(1)
-kit.servo[1].angle = 90
+kit.servo[1].angle = 180
 time.sleep(1)
 
 print("Two-servo test complete")
