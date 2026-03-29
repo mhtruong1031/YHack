c5dac203d70e7d12ec0863431a50c356418d8c3a
Teresa Nguyen <teresa.nguyen@yale.edu>
2026-03-28 22:10:35 -0700

    gpio test

---
+++ hardware/gpio_hp_test.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+TRIG = 23
+ECHO = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+GPIO.output(TRIG, False)
+time.sleep(2)
+
+try:
+    while True:
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        print("ECHO state right after trigger:", GPIO.input(ECHO))
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
