8836a8a067364c3aa01b5e4dbcbfbd7a27568703
Teresa Nguyen <teresa.nguyen@yale.edu>
2026-03-28 19:01:42 -0700

    servo Test

--- hardware/servo_test.py
+++ hardware/servo_test.py
@@ -2,18 +2,27 @@ from adafruit_servokit import ServoKit
 import time
 
 kit = ServoKit(channels=16, address=0x40)
-print("HELP!")
 
-kit.servo[0].angle = 0
+# center both
+kit.servo[0].angle = 90
+kit.servo[1].angle = 90
+time.sleep(2)
+
+# servo 0
+kit.servo[0].angle = 80
+time.sleep(1)
+kit.servo[0].angle = 100
+time.sleep(1)
+kit.servo[0].angle = 90
 time.sleep(1)
 
-kit.servo[0].angle = 180
+# servo 1
+kit.servo[1].angle = 80
+time.sleep(1)
+kit.servo[1].angle = 100
+time.sleep(1)
+kit.servo[1].angle = 90
 time.sleep(1)
 
-kit.servo[0].angle = 0
-time.sleep(1)
-
-kit.servo[0].angle = 180
-time.sleep(1)
-
+print("Two-servo test complete")
 
