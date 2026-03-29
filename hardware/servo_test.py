c95ec365c30996057e0a82b9fdf9997079e4be76
teresanguyen01 <teresatamnguyen20@gmail.com>
2026-03-28 21:53:50 -0400

    servo_test

---
+++ hardware/servo_test.py
@@ -0,0 +1,19 @@
+from adafruit_servokit import ServoKit
+import time
+
+kit = ServoKit(channels=16, address=0x40)
+print("HELP!")
+
+kit.servo[0].angle = 0
+time.sleep(1)
+
+kit.servo[0].angle = 180
+time.sleep(1)
+
+kit.servo[0].angle = 0
+time.sleep(1)
+
+kit.servo[0].angle = 180
+time.sleep(1)
+
+
